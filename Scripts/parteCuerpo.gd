75c93a45c2340727a1e35d2f3ca8916d5ac154b0
Nicolas <nrodrigueo@bancochile.cl>
2021-01-31 01:21:18 -0300

    poc

--- Scripts/parteCuerpo.gd
+++ Scripts/parteCuerpo.gd
@@ -1,9 +1,14 @@
-extends Node2D
+extends KinematicBody2D;
 
 export var piezanombre = ""
 export var cantidad = 1
+export var estadoColision = false;
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("player"):
 		body.add_pieza(piezanombre,cantidad)
 		queue_free()
+
+func colisionTrampa():
+	if(!estadoColision):
+		print('oli')
